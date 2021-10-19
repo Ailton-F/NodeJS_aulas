bbe4ff8b900f506b77bb5143ca52c65b6baa208c
Ailton-F <ailtonxdz@gmail.com>
2021-10-19 11:34:25 -0300

    Módulo 3 terminado

--- M3/index.js
+++ M3/index.js
@@ -22,7 +22,9 @@ const Post = require('./models/Post')
 
 //Rotas
     app.get('/', function(req, res){
-      res.render('home')
+      Post.findAll({order: [['id', 'DESC']]}).then(function(posts){//Pega todos os posts, o 'order' organiza ele pede dois parâmetros, o primeiro é a partir de que vc quer ordenar, e o outro como você deseja fazer isso
+        res.render('home', {posts:posts.map(posts => posts.toJSON())})//Renderiza os posts na variavel 'posts'
+      })
     })
 
     app.get('/cad', function(req, res){//O método getcria uma sequência de consulta(query string) e a acrescenta à URL do script no servidor que manipula a solicitação.
@@ -37,6 +39,16 @@ const Post = require('./models/Post')
         res.redirect('/')//'redirect' me direciona a uma rota
       }).catch(function(error){
         res.send(`Falha ao criar: ${error}`)
+      })
+    })
+
+    app.get('/deletar/:id', function(req, res){
+      Post.destroy(
+        {where: {'id': req.params.id}//Faço a requisição do parâmetro da rota.
+      }).then(()=>{
+        res.send('Postagem deletada com sucesso')
+      }).catch(function(error){
+        res.send('Está postagem não existe')
       })
     })
 
